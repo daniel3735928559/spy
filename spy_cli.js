a48cadf63935231714481a01122bb5274c049cd0
DR <daniel3735928559@gmail.com>
2015-10-05 15:49:29 -0500

    do a better nothing

--- spy_cli.js
+++ spy_cli.js
@@ -4,7 +4,7 @@ benv.setup(function(){
     benv.expose({angular:benv.require('./lib/angular/angular.js','angular')});
     var app = angular.module('app', [])
     app.controller("ClController", ['$scope',function($scope){
-	$scope.prog = "x = 27";
+	$scope.prog = "x = 27\ny = 28\n z= x+y";
 	$scope.pycontrol = {};
     }]);
     var parser = require('./spy/expr_node.js').parser;
@@ -17,18 +17,34 @@ benv.setup(function(){
     var $controller = angular.injector(['ng', 'app']).get('$controller');
     var scope = {};
     $controller('PySimController', { $scope: scope });
-    scope.program = 'x = 27+1';
+    scope.program = 'x = 27+1\nwhile(x > 10):\n   x = x-1\n   y = 28\nz= x+y\nz= x+y\nz= x+y\nz= x+y\nz= x+y';
     scope.editor = {
 	getValue:function(){return scope.program;},
 	setOption:function(){return;},
+	addLineClass:function(){return;},
 	removeLineClass:function(){return;},
 	clearGutter:function(){return;},
 	setGutterMarker:function(){return;}
     }
     scope.run()
     scope.p = parser;
-    scope.step()
+    scope.step();
+    scope.step();
+    scope.step();
+    scope.step();
+    scope.step();
+    scope.step();
+    scope.step();
+    scope.step();
+    scope.step();
+    scope.step();
+    scope.step();
+    scope.step();
+    scope.step();
+    scope.step();
+    scope.step();
     console.log(scope.variables);
+    console.log(scope.status);
 });
 // var path = require('path');
 // require(path.join(path.resolve('./'),process.argv[2]))();
